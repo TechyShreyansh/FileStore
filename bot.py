3a584daf0608270b228b5bf6a7b24b156a59fb7b
Tech Shreyansh <165946497+TechyShreyansh@users.noreply.github.com>
2025-10-25 15:20:58 +0530

    Updated By Tech Shreyansh

---
+++ bot.py
@@ -0,0 +1,94 @@
+from aiohttp import web
+from plugins import web_server
+import asyncio
+import pyromod.listen
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+import sys
+from datetime import datetime
+#MrGhostsx on Tg
+from config import *
+
+
+name ="""
+ BY MrGhostsx
+"""
+
+def get_indian_time():
+    """Returns the current time in IST."""
+    ist = pytz.timezone("Asia/Kolkata")
+    return datetime.now(ist)
+
+
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_hash=API_HASH,
+            api_id=APP_ID,
+            plugins={
+                "root": "plugins"
+            },
+            workers=TG_BOT_WORKERS,
+            bot_token=TG_BOT_TOKEN
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = get_indian_time()
+
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Tech_Shreyansh2 for support")
+            sys.exit()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Tech_Shreyansh2")
+        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @Tech_Shreyansh29""")
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.username = usr_bot_me.username
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @Tech_Shreyansh29")   
+
+        # Start Web Server
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
+
+
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Tech_Shreyansh29</blockquote></b>")
+        except: pass
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
+
+    def run(self):
+        """Run the bot."""
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @MrGhostsx")
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            self.LOGGER(__name__).info("Shutting down...")
+        finally:
+            loop.run_until_complete(self.stop())
+
+#
+# Copyright (C) 2025 by TechyShreyansh@Github, < https://github.com/TechyShreyansh >.
+#
+# This file is part of < https://github.com/TechyShreyansh > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TechyShreyansh/blob/master/LICENSE >
+#
+# All rights reserved.
